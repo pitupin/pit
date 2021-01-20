d05c3d21b0671defe18164822496106255a09ec7
pitupin <76623070+pitupin@users.noreply.github.com>
2021-01-20 21:09:01 +0300

    Add files via upload

---
+++ Project/Project/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.изменитьЦветToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.Location = new System.Drawing.Point(227, 45);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(500, 400);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Location = new System.Drawing.Point(59, 62);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(75, 23);
+            this.buttonUp.TabIndex = 1;
+            this.buttonUp.Text = "Up";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(13, 105);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(75, 23);
+            this.buttonLeft.TabIndex = 2;
+            this.buttonLeft.Text = "Left";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(114, 105);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(75, 23);
+            this.buttonRight.TabIndex = 3;
+            this.buttonRight.Text = "Right";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Location = new System.Drawing.Point(59, 151);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(75, 23);
+            this.buttonDown.TabIndex = 4;
+            this.buttonDown.Text = "Down";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.изменитьЦветToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip.TabIndex = 5;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // изменитьЦветToolStripMenuItem
+            // 
+            this.изменитьЦветToolStripMenuItem.Name = "изменитьЦветToolStripMenuItem";
+            this.изменитьЦветToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.изменитьЦветToolStripMenuItem.Text = "изменить цвет";
+            this.изменитьЦветToolStripMenuItem.Click += new System.EventHandler(this.изменитьЦветToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem изменитьЦветToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog;
+    }
+}
+
---
+++ Project/Project/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        
+        int xStart, yStart ;
+        
+
+        Graphics graphics;
+        Bitmap currentBitmap;
+        Bitmap saveBitmap;
+       
+        SolidBrush brush;
+       
+        int size;
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            
+           yStart  = yStart - size;
+            if (yStart <= 0)
+            {
+                yStart = 0;
+            }
+
+            currentBitmap = (Bitmap)saveBitmap.Clone();
+            graphics = Graphics.FromImage(currentBitmap);
+
+            graphics.FillRectangle(brush, xStart, yStart, size, size);
+            pictureBox.Image = currentBitmap;
+
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            xStart = xStart - size;
+            if (xStart <= 0)
+            {
+                xStart = 0 ;
+            }
+
+            currentBitmap = (Bitmap)saveBitmap.Clone();
+            graphics = Graphics.FromImage(currentBitmap);
+
+            graphics.FillRectangle(brush, xStart, yStart, size, size);
+            pictureBox.Image = currentBitmap;
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            xStart = xStart + size;
+            if (xStart + size >= pictureBox.Width)
+            {
+                xStart = pictureBox.Width - size;
+            }
+            currentBitmap = (Bitmap)saveBitmap.Clone();
+            graphics = Graphics.FromImage(currentBitmap);
+
+            graphics.FillRectangle (brush,  xStart, yStart, size, size);
+            pictureBox.Image = currentBitmap; 
+            
+        } 
+
+
+        private void изменитьЦветToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK);
+            {
+                 brush.Color = colorDialog.Color;
+                изменитьЦветToolStripMenuItem.BackColor = colorDialog.Color;
+                currentBitmap = (Bitmap)saveBitmap.Clone();
+                graphics = Graphics.FromImage(currentBitmap);
+
+                graphics.FillRectangle(brush, xStart, yStart, size, size);
+                pictureBox.Image = currentBitmap;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            currentBitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            saveBitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            graphics = Graphics.FromImage(currentBitmap);
+           
+            brush = new SolidBrush(Color.Red);
+
+           
+            yStart = 1;
+            xStart = 1;
+            size = 100;
+            graphics.FillRectangle(brush, xStart, yStart, size, size);
+            pictureBox.Image = currentBitmap;
+            brush = new SolidBrush(colorDialog.Color);
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            yStart = yStart + size;
+            if (yStart + size  >= pictureBox.Height)
+            {
+                yStart =  pictureBox.Height - size;
+            }
+
+            currentBitmap = (Bitmap)saveBitmap.Clone();
+            graphics = Graphics.FromImage(currentBitmap);
+
+            graphics.FillRectangle(brush, xStart, yStart, size, size);
+            pictureBox.Image = currentBitmap;
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
